c4a0bb5430bc3928977a8beb33e5bfa1033e38da
YaBoiLio <elliot.rhodes5002@outlook.com>
2025-12-06 14:28:42 +0000

    Noelle update

--- noelle.gd
+++ noelle.gd
@@ -5,8 +5,9 @@ const SPEED = 300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+var facingDirection := "forward"
+
 func _physics_process(delta: float) -> void:
-	var facingDirection := "forward"
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var xDirection := Input.get_axis("ui_left", "ui_right")
