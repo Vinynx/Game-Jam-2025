08c657eb63af83bc009171853678af9882be4d8d
Vinynx <GalacticWolf306@gmail.com>
2025-12-06 19:09:53 +0000

    menu (minus credits)

--- main_menu.gd
+++ main_menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _ready():
 	%Play.pressed.connect(play)
+	%Quit.pressed.connect(quit_game)
 
 func play():
 	get_tree().change_scene_to_file('res://overworld.tscn')
+
+func quit_game():
+	get_tree().quit()
