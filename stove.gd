2afe540a3e61d918f79f0a417923bfeea74633b0
Vinynx <GalacticWolf306@gmail.com>
2025-12-07 08:08:23 +0000

    started cooking minigame

--- stove.gd
+++ stove.gd
@@ -1,18 +1,40 @@
 extends Control
 
 var timer := Timer.new()
+var tracker = 1
+var count = 0
+var score = 0
+var started = false
 
 func _ready() -> void:
 	add_child(timer)
-	timer.wait_time = 1.0
+	timer.wait_time = 0.6
 	timer.one_shot = false
 	timer.timeout.connect(_on_timer_timeout)
-	timer.start()
+	%Start.pressed.connect(start)
+			
+func _physics_process(delta: float) -> void:
+	if tracker == 2 && started == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			score += 1
+		print(score)
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("ui_cancel") || Input.is_action_pressed("ui_x"):
 		get_tree().change_scene_to_file('res://overworld.tscn')
-
+		
+func start():
+	timer.start()
+	started = true
+	%Start.hide()
 
 func _on_timer_timeout():
-	print("test")
+	if tracker == 1:
+		%Indicator.show()
+		tracker = 2
+	elif tracker == 2:
+		%Indicator.hide()
+		tracker = 1
+	count += 1
+	if count == 10:
+		timer.stop()
