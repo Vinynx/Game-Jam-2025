c53c8decee9b1a7aaf859b3b2e7c47ed6abceb6e
Vinynx <GalacticWolf306@gmail.com>
2025-12-07 12:02:57 +0000

    4

--- fridge.gd
+++ fridge.gd
@@ -10,7 +10,7 @@ func _ready():
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("ui_cancel") || Input.is_action_pressed("ui_x"):
 		if Global.reset:
-			get_tree().quit()
+			get_tree().change_scene_to_file('res://startup.tscn')
 		else:
 			get_tree().change_scene_to_file('res://overworld.tscn')
 
--- startup.gd
+++ startup.gd
@@ -2,8 +2,14 @@ extends Control
 
 var counter = 1
 
+func _ready() -> void:
+	if Global.reset:
+		%Talk3.show()
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("ui_text_completion_accept") || Input.is_action_pressed("left_click") || Input.is_action_pressed("ui_z"):
+		if Global.reset:
+			get_tree().quit()
 		if counter == 1:
 			%Talk1.hide()
 			counter += 1
